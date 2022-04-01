717b414708cf34deec2126c462aa43e462dfffa5
Rochdi Dardor <72025083+Drr31@users.noreply.github.com>
2022-04-01 17:13:39 +0200

    Update bank.py

--- bank.py
+++ bank.py
@@ -3,19 +3,25 @@ class Banque:
         self.nom = nom
         self.swift = swift
 
+
+
 class Client:
-    def init(self, idCient = NULL, nom = "none", adresse ="none", motDePasse=NULL):
+
+    def init(self, idCient = None, nom = "none", adresse ="none", motDePasse=None):
         self.idClient = idCient
         self.nom = nom
         self.adresse = adresse
         self.motDePasse = motDePasse
+
     def identifier(self, motDePasse):
         if self.motDePasse == motDePasse:
             print("access garenteed")
         else:
             print("access denied")
 
+
+
 class Compte:
-    def init(self, Numero = NULL, Solde = 0):
+    def __init__(self, Numero = None, Solde = 0):
         self.Numero = Numero
         self.Solde = Solde
